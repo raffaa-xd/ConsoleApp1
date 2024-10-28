bd92baad122bb79639d692ea8de9aa17c097d364
Kab-35-16 <Kab-35-16@KAB-31-11->
2024-10-28 12:27:01 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Найти сумму двух целых чисел
+            Console.Write($"number_1= ");
+            int number_1 = int.Parse(Console.ReadLine());
+            Console.Write($"number_2= ");
+            int number_2 = int.Parse(Console.ReadLine());
+            int sum = number_1 + number_2;
+            Console.Write($"sum= {sum}");
+            Console.Read();
+        }
+    }
+}
